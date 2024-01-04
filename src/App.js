3d68003e3956d1ec6d27f7467805b643713377d7
katherinex <kathexio@gmail.com>
2024-01-04 00:03:24 -0500

    Implementar header ktxio-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app__header">
+        <div></div>
+        <figure>
+          <img src="KTXIO_Negative_Brand.png" alt="ktxio logo"></img>
+        </figure>
       </header>
+      <section className="app__main-section"></section>
     </div>
+    
   );
 }
 
