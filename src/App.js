bbde931ab3f41c1953574311141d603524fb3746
katherinex <kathexio@gmail.com>
2024-01-04 20:08:12 -0500

    Primera sección

--- src/App.js
+++ src/App.js
@@ -4,12 +4,44 @@ function App() {
   return (
     <div className="app">
       <header className="app__header">
-        <div></div>
-        <figure>
-          <img src="KTXIO_Negative_Brand.png" alt="ktxio logo"></img>
-        </figure>
+        <img src="KTXIO_Negative_Brand.png" alt="ktxio logo"></img>
       </header>
-      <section className="app__main-section"></section>
+      <section className="app__main-section">
+        <div className="app__main-section__main-tag">
+          <span>
+            {'<main>'}
+          </span>
+          <h1>
+            I AM KATHE~
+          </h1>
+          <h2>
+            Electronics & <br/>
+            Telecomunications Engineer
+          </h2>
+          <p>
+            I’m interested in web & mobile application 
+            development, accessibility, make end2end 
+            tests, and get involved in front-end projects.
+          </p>
+
+          <button>
+            View projects
+          </button>
+          <span>
+            {'</main>'}
+          </span>
+        </div>
+        <div className="app__main-section__main-photo">
+          <figure >
+            <div className="app__main-section__main-photo__oval"></div>
+            <img src="Kathe_photo.png" alt="kathe" />
+          </figure>
+
+        </div>
+        
+
+     
+      </section>
     </div>
     
   );
